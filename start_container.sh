476a0098350b810dca7f2fe310f2a87cc6096b33
vigneshmv14398 <115076422+vigneshmv14398@users.noreply.github.com>
2025-06-18 07:42:53 +0530

    Delete start_container.sh

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull vigneshmv14398/flask-cicd-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 vigneshmv14398/flask-cicd-app
