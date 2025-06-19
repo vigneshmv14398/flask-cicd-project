06801e00d778ed3e78d2fbea19cef912b6d20701
vigneshmv14398 <115076422+vigneshmv14398@users.noreply.github.com>
2025-06-19 08:45:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull vigneshmv14398/flask-cicd-app
 
-# Run the Docker image as a container
+# Run the Docker image as a container 
 docker run -d -p 5000:5000 vigneshmv14398/flask-cicd-app
