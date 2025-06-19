9cf9c2f49c097c483216fdf0ed5bdab2f573e4a9
Vigneshvaran <vigneshmv14398@gmail.com>
2025-06-19 08:36:23 +0530

    new one

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Cleaning up old app directory..."
-mkdir -p /home/ec2-user/flask-app
-rm -rf /home/ec2-user/flask-app/*
-echo "Cleanup done."
+mkdir -p /opt/flask-app
+rm -rf /opt/flask-app/*
